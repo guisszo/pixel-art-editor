a8b5daad8b18cfe58744f02a98153cdcc86803ef
Guisszo <leguisse@gmail.com>
2025-06-14 16:41:55 +0200

    add new selector

--- features/pixelArts/gridPixelSelectors.ts
+++ features/pixelArts/gridPixelSelectors.ts
@@ -6,6 +6,10 @@ export const getGrid = (state: RootState):Pixel[][] => state.pixelArt.grid;
 export const getSelectedTool = (state: RootState): string => state.pixelArt.selectedTool;
 export const getPast = (state: RootState): Pixel[][][] => state.pixelArt.past;
 export const getFuture = (state: RootState): Pixel[][][] => state.pixelArt.future;
+export const getGridRows = (state: RootState) => state.pixelArt.gridRows;
+export const getGridCols = (state: RootState) => state.pixelArt.gridCols;
+export const getCellSize = (state: RootState) => state.pixelArt.cellSize;
+export const getZoomLevel = (state: RootState) => state.pixelArt.zoomLevel;
 export const isGridEmpty = (grid:Pixel[][]) => {
 
     if (!grid || grid.length === 0) return true;
